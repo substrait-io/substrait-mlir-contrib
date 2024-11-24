d2fbc62b92bf06f29ad61d92666cb63c11fd8475
Ingo Müller <ingomueller@google.com>
2024-11-24 15:22:41 +0100

    ci: add check to enforce presence of license identifier (#7)
    Also add license header to lit.cfg.py. That file doesn't seem to be
    particularly important to specify the license for but our CI check
    complains about it and I guess it cannot harm either.
    
    Signed-off-by: Ingo Müller <ingomueller@google.com>

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -1,3 +1,11 @@
+# ===-- lif.cfg.py - Configuration for LIT unit tests ---------------------=== #
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# ===----------------------------------------------------------------------=== #
+
 import os
 
 import lit.formats
