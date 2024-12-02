a459ad3e3a2298bf81ef4f4873caee057c6fc4c0
Dalia Shaaban <dalia.shaaban08@gmail.com>
2024-12-02 16:34:01 +0100

    not sure how this got lost along the way

--- lib/Target/SubstraitPB/Import.cpp
+++ lib/Target/SubstraitPB/Import.cpp
@@ -276,6 +276,8 @@ importLiteral(ImplicitLocOpBuilder builder,
 
 static mlir::FailureOr<FetchOp> importFetchRel(ImplicitLocOpBuilder builder,
                                                const Rel &message) {
+  const FetchRel &fetchRel = message.fetch();
+
   // Import input.
   const Rel &inputRel = fetchRel.input();
 
