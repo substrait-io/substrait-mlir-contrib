146b50a563b0b0493a972a23788c518be8773cda
Ingo Müller <ingomueller@google.com>
2024-12-09 13:41:52 +0100

    ci: run pyright as part of CI and fix issues it found (#22)
    This commit fixes some problems found by `pyright`:
    * In the DuckDB test, a `bytes` parameter is wrongly annotated as `str`
      in the stub files of that Python module. We silence that false
      positive with a suppression.
    * In the test for converting MLIR ops to plans, we use some op-specific
      members on an object whose most narrow known type is `MlirOperation`,
      so the commit adds a `cast` to make the `typing` information more
      precise.
    
    Signed-off-by: Ingo Müller <ingomueller@google.com>

--- test/python/dialects/substrait/e2e_duckdb.py
+++ test/python/dialects/substrait/e2e_duckdb.py
@@ -49,7 +49,8 @@ def testNamedTable():
   pb_plan = ss.to_binpb(plan.operation).encode()
 
   # Execute in duckdb and print result.
-  query_result = con.from_substrait(proto=pb_plan)
+  # Ignore type because DuckDB's `from_substrait` has wrong type annotation.
+  query_result = con.from_substrait(proto=pb_plan)  # type: ignore
 
   print(query_result.to_arrow_table())
   # CHECK-NEXT:          pyarrow.Table
