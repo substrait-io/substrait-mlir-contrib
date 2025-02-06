76ab7c149f3ddc705d96cb0432397ddf8ddf18a3
Ingo Müller <ingomueller@google.com>
2025-02-06 18:07:24 +0100

    chore: move free functions in `Substrait.cpp` towards top of file (#72)
    Signed-off-by: Ingo Müller <ingomueller@google.com>

--- lib/Dialect/Substrait/IR/Substrait.cpp
+++ lib/Dialect/Substrait/IR/Substrait.cpp
@@ -141,6 +141,103 @@ static void printAggregateRegions(OpAsmPrinter &printer, AggregateOp op,
 #define GET_OP_CLASSES
 #include "substrait-mlir/Dialect/Substrait/IR/SubstraitOps.cpp.inc"
 
+namespace {
+
+/// Computes the type of the nested field of the given `type` identified by
+/// `position`. Each entry `n` in the given index array `position` corresponds
+/// to the `n`-th entry in that level. The function is thus implemented
+/// recursively, where each recursion level extracts the type of the outer-most
+/// level identified by the first index in the `position` array.
+FailureOr<Type> computeTypeAtPosition(Location loc, Type type,
+                                      ArrayRef<int64_t> position) {
+  if (position.empty())
+    return type;
+
+  // Recurse into tuple field of first index in position array.
+  if (auto tupleType = llvm::dyn_cast<TupleType>(type)) {
+    int64_t index = position[0];
+    ArrayRef<Type> fieldTypes = tupleType.getTypes();
+    if (index >= static_cast<int64_t>(fieldTypes.size()) || index < 0)
+      return emitError(loc) << index << " is not a valid index for " << type;
+
+    return computeTypeAtPosition(loc, fieldTypes[index], position.drop_front());
+  }
+
+  return emitError(loc) << "can't extract element from type " << type;
+}
+
+/// Verifies that the provided field names match the provided field types. While
+/// the field types are potentially nested, the names are given in a single,
+/// flat list and correspond to the field types in depth first order (where each
+/// nested tuple-typed field has a name and its nested field have names on their
+/// own). Furthermore, the names on each nesting level need to be unique. For
+/// details, see
+/// https://substrait.io/tutorial/sql_to_substrait/#types-and-schemas.
+FailureOr<int> verifyNamedStructHelper(Location loc,
+                                       llvm::ArrayRef<Attribute> fieldNames,
+                                       TypeRange fieldTypes) {
+  int numConsumedNames = 0;
+  llvm::SmallSet<llvm::StringRef, 8> currentLevelNames;
+  for (Type type : fieldTypes) {
+    // Check name of current field.
+    if (numConsumedNames >= static_cast<int>(fieldNames.size()))
+      return emitError(loc, "not enough field names provided");
+    auto currentName = llvm::cast<StringAttr>(fieldNames[numConsumedNames]);
+    if (!currentLevelNames.insert(currentName).second)
+      return emitError(loc, llvm::Twine("duplicate field name: '") +
+                                currentName.getValue() + "'");
+    numConsumedNames++;
+
+    // Recurse for nested structs/tuples.
+    if (auto tupleType = llvm::dyn_cast<TupleType>(type)) {
+      llvm::ArrayRef<Type> nestedFieldTypes = tupleType.getTypes();
+      llvm::ArrayRef<Attribute> remainingNames =
+          fieldNames.drop_front(numConsumedNames);
+      FailureOr<int> res =
+          verifyNamedStructHelper(loc, remainingNames, nestedFieldTypes);
+      if (failed(res))
+        return failure();
+      numConsumedNames += res.value();
+    }
+  }
+  return numConsumedNames;
+}
+
+LogicalResult verifyNamedStruct(Operation *op,
+                                llvm::ArrayRef<Attribute> fieldNames,
+                                TupleType tupleType) {
+  Location loc = op->getLoc();
+  TypeRange fieldTypes = tupleType.getTypes();
+
+  // Emits error message with context on failure.
+  auto emitErrorMessage = [&]() {
+    InFlightDiagnostic error = op->emitOpError()
+                               << "has mismatching 'field_names' ([";
+    llvm::interleaveComma(fieldNames, error);
+    error << "]) and result type (" << tupleType << ")";
+    return error;
+  };
+
+  // Call recursive verification function.
+  FailureOr<int> numConsumedNames =
+      verifyNamedStructHelper(loc, fieldNames, fieldTypes);
+
+  // Relay any failure.
+  if (failed(numConsumedNames))
+    return emitErrorMessage();
+
+  // If we haven't consumed all names, we got too many of them, so report.
+  if (numConsumedNames.value() != static_cast<int>(fieldNames.size())) {
+    InFlightDiagnostic error = emitErrorMessage();
+    error.attachNote(loc) << "too many field names provided";
+    return error;
+  }
+
+  return success();
+}
+
+} // namespace
+
 namespace mlir {
 namespace substrait {
 
@@ -526,29 +623,6 @@ EmitOp::inferReturnTypes(MLIRContext *context, std::optional<Location> loc,
   return success();
 }
 
-/// Computes the type of the nested field of the given `type` identified by
-/// `position`. Each entry `n` in the given index array `position` corresponds
-/// to the `n`-th entry in that level. The function is thus implemented
-/// recursively, where each recursion level extracts the type of the outer-most
-/// level identified by the first index in the `position` array.
-static FailureOr<Type> computeTypeAtPosition(Location loc, Type type,
-                                             ArrayRef<int64_t> position) {
-  if (position.empty())
-    return type;
-
-  // Recurse into tuple field of first index in position array.
-  if (auto tupleType = llvm::dyn_cast<TupleType>(type)) {
-    int64_t index = position[0];
-    ArrayRef<Type> fieldTypes = tupleType.getTypes();
-    if (index >= static_cast<int64_t>(fieldTypes.size()) || index < 0)
-      return emitError(loc) << index << " is not a valid index for " << type;
-
-    return computeTypeAtPosition(loc, fieldTypes[index], position.drop_front());
-  }
-
-  return emitError(loc) << "can't extract element from type " << type;
-}
-
 LogicalResult FieldReferenceOp::inferReturnTypes(
     MLIRContext *context, std::optional<Location> loc, ValueRange operands,
     DictionaryAttr attributes, OpaqueProperties properties, RegionRange regions,
@@ -662,76 +736,6 @@ JoinOp::inferReturnTypes(MLIRContext *context, std::optional<Location> loc,
   auto resultType = TupleType::get(context, fieldTypes);
 
   inferredReturnTypes = SmallVector<Type>{resultType};
-
-  return success();
-}
-
-/// Verifies that the provided field names match the provided field types. While
-/// the field types are potentially nested, the names are given in a single,
-/// flat list and correspond to the field types in depth first order (where each
-/// nested tuple-typed field has a name and its nested field have names on their
-/// own). Furthermore, the names on each nesting level need to be unique. For
-/// details, see
-/// https://substrait.io/tutorial/sql_to_substrait/#types-and-schemas.
-static FailureOr<int>
-verifyNamedStructHelper(Location loc, llvm::ArrayRef<Attribute> fieldNames,
-                        TypeRange fieldTypes) {
-  int numConsumedNames = 0;
-  llvm::SmallSet<llvm::StringRef, 8> currentLevelNames;
-  for (Type type : fieldTypes) {
-    // Check name of current field.
-    if (numConsumedNames >= static_cast<int>(fieldNames.size()))
-      return emitError(loc, "not enough field names provided");
-    auto currentName = llvm::cast<StringAttr>(fieldNames[numConsumedNames]);
-    if (!currentLevelNames.insert(currentName).second)
-      return emitError(loc, llvm::Twine("duplicate field name: '") +
-                                currentName.getValue() + "'");
-    numConsumedNames++;
-
-    // Recurse for nested structs/tuples.
-    if (auto tupleType = llvm::dyn_cast<TupleType>(type)) {
-      llvm::ArrayRef<Type> nestedFieldTypes = tupleType.getTypes();
-      llvm::ArrayRef<Attribute> remainingNames =
-          fieldNames.drop_front(numConsumedNames);
-      FailureOr<int> res =
-          verifyNamedStructHelper(loc, remainingNames, nestedFieldTypes);
-      if (failed(res))
-        return failure();
-      numConsumedNames += res.value();
-    }
-  }
-  return numConsumedNames;
-}
-
-static LogicalResult verifyNamedStruct(Operation *op,
-                                       llvm::ArrayRef<Attribute> fieldNames,
-                                       TupleType tupleType) {
-  Location loc = op->getLoc();
-  TypeRange fieldTypes = tupleType.getTypes();
-
-  // Emits error message with context on failure.
-  auto emitErrorMessage = [&]() {
-    InFlightDiagnostic error = op->emitOpError()
-                               << "has mismatching 'field_names' ([";
-    llvm::interleaveComma(fieldNames, error);
-    error << "]) and result type (" << tupleType << ")";
-    return error;
-  };
-
-  // Call recursive verification function.
-  FailureOr<int> numConsumedNames =
-      verifyNamedStructHelper(loc, fieldNames, fieldTypes);
-
-  // Relay any failure.
-  if (failed(numConsumedNames))
-    return emitErrorMessage();
-
-  // If we haven't consumed all names, we got too many of them, so report.
-  if (numConsumedNames.value() != static_cast<int>(fieldNames.size())) {
-    InFlightDiagnostic error = emitErrorMessage();
-    error.attachNote(loc) << "too many field names provided";
-    return error;
-  }
 
   return success();
 }
